5e9a500dc74714d267beabf10644418fbe943a87
Fabio Vilela <fbvilela@gmail.com>
2015-08-03 01:31:52 +1000

    throwing some warning, will review this reggae later

--- model/url.rb
+++ model/url.rb
@@ -2,5 +2,5 @@ class Url < ActiveRecord::Base
   belongs_to :work
 
   validates :link, presence: true
-  validates :link, format: { with:  /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix}
+  #validates :link, format: { with:  /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix}
 end
