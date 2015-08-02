09a34812193b4048315d86bb96b3843a9bc7fa83
Fabio Vilela <fbvilela@gmail.com>
2015-08-03 01:23:15 +1000

    all the missing bits added. Needs lots of refactoring

--- run.rb
+++ run.rb
@@ -1,25 +1,42 @@
 require 'rubygems'
 require 'active_record'
 require_relative 'model/parser/works_parser'
-
+require 'erb'
+require_relative 'lib/page_generator'
 ActiveRecord::Base.logger = Logger.new(File.open('database.log', 'w'))
 ActiveRecord::Base.establish_connection(
   adapter:  "sqlite3",
   database: "database/works.db"
 )
+Work.connection
 
-xml_file = ARGV[0]
-destination = ARGV[1]
+xml_file = ARGV[0] || 'resources/works.xml'
+output_folder = ARGV[1] || 'output'
 
 unless File.exists?( xml_file )
-  puts "File #{xml_file} not found"
+  puts "Requested file #{xml_file} not found"
+  exit(0)
 end
 
-
+puts "Checking if database exists..."
 unless ActiveRecord::Base.connection.tables.include? 'works'
+  puts "Running database migrations"
   ActiveRecord::Migrator.migrate( 'db/migrate', ENV["VERSION"] ? ENV["VERSION"].to_i : nil)
 end
 
+puts "Parsing XML..."
 works_xml = IO.read(xml_file)
 works = Parser::WorksParser.parse( works_xml )
+puts "Saving into the database..."
 works.persist
+puts "Ready to generate pages..."
+
+# create index
+page_generator = PageGenerator.new( output_folder )
+puts "Generating index..."
+page_generator.generate_index
+
+puts "Generating camera makes pages..."
+page_generator.generate_camera_makes
+puts "Generating camera models pages..."
+page_generator.generate_camera_models
