76aea83631ef5983693a91af2882b736011e94b5
Fabio Vilela <fbvilela@gmail.com>
2015-08-05 03:05:40 +1000

    minor adjustments and default scope added

--- model/camera_make.rb
+++ model/camera_make.rb
@@ -1,10 +1,11 @@
 class CameraMake < ActiveRecord::Base
+  default_scope { order('name asc') } 	
   has_many :camera_models
   has_many :works , through: :camera_models
 
   validates :name, presence: true
 
   def latest_works
-    works.take(10)
+    works.latest
   end
 end
